1a893b336819e09d34cd0d3454121324821b6710
hebbar <prajwalhebbaras@gmail.com>
2024-12-22 19:53:41 +0530

    add values and for loop
    Added the suits and values array that will contain the values and the
    suits required for the cards. Used a for loop to loop through these and
    get the list of all possible combinations that are available. Used the
    format macros to join and format the list of combinations that is being
    generated.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ struct Deck {
 }
 
 fn main() {
+    let suits = ["Hearts", "Spades"];
+    let values = ["Ace", "One", "Two", "Three", "Four"];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+        }
+    }
+
     let deck = Deck { cards: vec![] };
     println!("Here is the deck: {:?}", deck)
 }
