9beefce0f3cb749490da766ab61686e8a419bff4
hebbar <prajwalhebbaras@gmail.com>
2024-12-23 17:27:35 +0530

    update the deal function
    Added a deal function which will take the complete deck of cards and
    gives a particular number of cards that are mentioned.

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,10 @@ impl Deck {
     fn shuffle(&mut self) {
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng);
-        //
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
     }
 }
 
@@ -33,6 +36,8 @@ fn main() {
     let mut deck = Deck::new();
 
     deck.shuffle();
+    let cards = deck.deal(3);
 
-    println!("Here is the deck: {:#?}", deck)
+    println!("Here is the hand: {:#?}", cards);
+    println!("Here is the deck: {:#?}", deck);
 }
